1d8ece830e2d336bc00c988d0c6ee26d413d24f4
edwin moradian <edwin.moradian90@gmail.com>
2020-03-25 14:19:23 -0700

    adding readme and stickler

--- src/modules/controllers/projectController.js
+++ src/modules/controllers/projectController.js
@@ -1,58 +1,55 @@
 import { getProjectStorage, setProjectStorage } from "./storageController";
 
-let projects = JSON.parse(localStorage.getItem('projects')) || [];
+let projects = JSON.parse(localStorage.getItem("projects")) || [];
 
 let state = {
-    projectOpened: false,
-    projectId: ''
+  projectOpened: false,
+  projectId: ""
 };
 
-const projectFactory = function(title, tasks=[]) {
-    return { title, tasks };
+const projectFactory = function(title, tasks = []) {
+  return { title, tasks };
 };
 
 const create = function(title) {
-    const newProject = projectFactory(title)
-    projects.push(newProject);
-    setProjectStorage();
-    console.log(projects)
+  const newProject = projectFactory(title);
+  projects.push(newProject);
+  setProjectStorage();
 };
 
 const deleteProject = function() {
-    projects.splice(state.projectId, 1);
-}
+  projects.splice(state.projectId, 1);
+};
 
 const projectsIndex = function() {
-    console.log(projects)
-    return projects;
+  return projects;
 };
 
 const getProjectState = function() {
-    return state;
+  return state;
 };
 
-const setProjectState = function(bool, id='') {
-    state = {
-        projectOpened: bool,
-        projectId: id,
-    };
+const setProjectState = function(bool, id = "") {
+  state = {
+    projectOpened: bool,
+    projectId: id
+  };
 };
 
 const deleteTask = function(task_id) {
-    projects[state.projectId].tasks.splice(task_id, 1);
+  projects[state.projectId].tasks.splice(task_id, 1);
 };
 
 const showTask = function(task_id) {
-    return projects[state.projectId].tasks[task_id];
+  return projects[state.projectId].tasks[task_id];
 };
 
-console.log(state.projectOpened)
 export {
-    create,
-    deleteProject, 
-    projectsIndex, 
-    getProjectState, 
-    setProjectState, 
-    deleteTask,
-    showTask,
+  create,
+  deleteProject,
+  projectsIndex,
+  getProjectState,
+  setProjectState,
+  deleteTask,
+  showTask
 };
--- src/modules/controllers/storageController.js
+++ src/modules/controllers/storageController.js
@@ -1,28 +1,22 @@
-import { index } from './todoController';
-import { projectsIndex } from './projectController';
+import { index } from "./todoController";
+import { projectsIndex } from "./projectController";
 
 const setStorage = function() {
-    const todos = index();
-    localStorage.setItem('todos', JSON.stringify(todos));
+  const todos = index();
+  localStorage.setItem("todos", JSON.stringify(todos));
 };
 
 var setProjectStorage = function() {
-    const projects = projectsIndex();
-    console.log(projects);
-    localStorage.setItem('projects', JSON.stringify(projects));
-}
+  const projects = projectsIndex();
+  localStorage.setItem("projects", JSON.stringify(projects));
+};
 
 const getStorage = function() {
-    return JSON.parse(localStorage.getItem('todos')) || [];
+  return JSON.parse(localStorage.getItem("todos")) || [];
 };
 
 var getProjectStorage = function() {
-    return JSON.parse(localStorage.getItem('projects')) || [];
+  return JSON.parse(localStorage.getItem("projects")) || [];
 };
 
-export { 
-    setStorage, 
-    getStorage, 
-    setProjectStorage, 
-    getProjectStorage, 
-};
+export { setStorage, getStorage, setProjectStorage, getProjectStorage };
